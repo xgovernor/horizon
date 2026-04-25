6670f3f4492a51c9fe90780fafae57652e9d380b
xgovernor <abut1081@gmail.com>
2026-04-25 20:10:35 +0600

    feat: initialize project structure with Vim-inspired UI, search functionality, and blog support using Astro and Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig( {
+  compressHTML: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: true,
+    },
+  },
+  integrations: [mdx()],
+});
---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    author: z.string().optional(),
+    summary: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
